3f56c274ff6fe2f62d15ab0fe4e6c94cae689c7b
Cliente CONTROL-PC <CltControl@GGIECUT1WRK030.espol.edu.ec>
2018-12-11 12:42:08 -0500

    cREACION DE BUILDER

--- ManejoComputadoras/src/emsamablajecomputadoras/creacional/BuilderComputer.java
+++ ManejoComputadoras/src/emsamablajecomputadoras/creacional/BuilderComputer.java
@@ -1,5 +1,17 @@
 package emsamablajecomputadoras.creacional;
 
-public class BuilderComputer {
+public abstract class BuilderComputer {
+   protected Computador computador;
+   
+   public Computador getComputador(){
+	   return computador;
+   }
+   public void crearComputador() {
+	   computador = new Computador();
+   }
+   
+   public abstract void buildMainboard();
+   public abstract void buildSOperativo();
+   
 
 }
